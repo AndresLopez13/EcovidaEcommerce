b1a47253afb738d6eb9d520d3937c21b99b5e071
AndresLopez13 <andresilopeza@gmail.com>
2024-06-23 23:32:45 -0500

    refactor routes and find product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,10 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.all.with_attached_image
   end
 
   def show
-    @product = Product.find(params[:id])
+    product
   end
 
   def new
@@ -23,13 +23,11 @@ class ProductsController < ApplicationController
   end
 
   def edit
-    @product = Product.find(params[:id])
+    product
   end
 
   def update
-    @product = Product.find(params[:id])
-
-    if @product.update(product_params)
+    if product.update(product_params)
       redirect_to products_path, notice: 'Producto actualizado correctamente'
     else
       render :edit, status: :unprocessable_entity # devuelve un status 422 (Unprocessable Content)
@@ -37,9 +35,7 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    @product = Product.find(params[:id])
-    @product.destroy
-
+    product.destroy
     redirect_to products_path, notice: 'Producto eliminado correctamente'
   end
 
@@ -47,5 +43,9 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:title, :description, :price, :image)
+  end
+
+  def product
+    @product = Product.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,3 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  # get "up" => "rails/health#show", as: :rails_health_check
-  get 'products', to: 'products#index'
-  get '/products/new', to: 'products#new', as: :new_product
-  post '/products', to: 'products#create'
-  patch '/products/:id', to: 'products#update'
-  get '/products/:id', to: 'products#show', as: :product
-  get '/products/:id/edit', to: 'products#edit', as: :edit_product
-  delete '/products/:id', to: 'products#destroy'
-  # Defines the root path route ("/")
-  root 'products#index'
+  resources :products, path: '/'
 end
